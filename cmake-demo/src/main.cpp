e3665290657b8851fbd0d30e8a1254a638bdb4a7
PremoRim <3265495643@qq.com>
2026-03-05 14:54:21 +0800

    完成第六天学习任务+笔记整理

--- cmake-demo/src/main.cpp
+++ cmake-demo/src/main.cpp
@@ -1,6 +1,6 @@
 #include "hello.h"  // 包含自定义头文件
 #include <iostream>
-
+#include <unistd.h>
 int main() {
     // 调用打印函数（模拟启动AI推理工具）
     print_hello("LLM-Inference-Tool");
@@ -9,5 +9,11 @@ int main() {
     int a = 10, b = 20;
     std::cout << "基础计算示例：" << a << " + " << b << " = " << add(a, b) << std::endl;
     
+    std::cout << "程序进入常驻循环模式，按下Ctrl+C退出..." << std::endl;
+    int t = 1;
+    while (true) {
+        sleep(1);  // 每秒休眠一次，不占CPU
+        std::cout << t++ << std::endl;
+    }
     return 0;
 }
